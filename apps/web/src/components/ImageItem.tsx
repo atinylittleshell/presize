790978c00a92ffb1db4cfff577737cb9c5c5f7e7
Timothy Cho <104095972+timothycho01@users.noreply.github.com>
2025-05-18 00:34:41 -0400

    Update ImageItem.tsx - fix icon size

--- apps/web/src/components/ImageItem.tsx
+++ apps/web/src/components/ImageItem.tsx
@@ -55,8 +55,8 @@ export function TbRotate(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="1em"
+      height="1em"
       viewBox="0 0 24 24"
       strokeWidth="2"
       stroke="currentColor"
